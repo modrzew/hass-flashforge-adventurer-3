c4e41098f85ace9b35e4888192377d0c6d6c037d
Darek Margas <71180450+darek-margas@users.noreply.github.com>
2024-08-25 11:23:25 +1000

    Fixing regex handling

--- custom_components/flashforge_adventurer/protocol.py
+++ custom_components/flashforge_adventurer/protocol.py
@@ -13,8 +13,8 @@ STATUS_COMMAND = '~M601 S1'
 PRINT_JOB_INFO_COMMAND = '~M27'
 TEMPERATURE_COMMAND = '~M105'
 
-STATUS_REPLY_REGEX = re.compile('CMD M27 Received.\r\n\w+ printing byte (\d+)/100\r\nLayer: (\d+)/(\d+)\r\nok\r\n')
-TEMPERATURE_REPLY_REGEX = re.compile('CMD M105 Received.\r\nT0:(\d+)\W*/(\d+) B:(\d+)\W*/(\d+)\r\nok\r\n')
+STATUS_REPLY_REGEX = re.compile(r'CMD M27 Received.\r\n\w+ printing byte (\d+)/100\r\nLayer: (\d+)/(\d+)\r\nok\r\n')
+TEMPERATURE_REPLY_REGEX = re.compile(r'CMD M105 Received.\r\nT0:(\d+)\W*/(\d+) B:(\d+)\W*/(\d+)\r\nok\r\n')
 
 class PrinterStatus(TypedDict):
     online: bool
@@ -57,6 +57,8 @@ def parse_data(response: PrinterStatus, print_job_info: str, temperature_info: s
         response['progress'] = int(print_job_info_match.group(1))
         response['printing_layer'] = int(print_job_info_match.group(2))
         response['total_layers'] = int(print_job_info_match.group(3))
+
+    
     temperature_match = TEMPERATURE_REPLY_REGEX.match(temperature_info)
     if temperature_match:
         # Printer is printing if desired temperatures are greater than zero. If not, it's paused.
