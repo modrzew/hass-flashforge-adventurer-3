6f4f298d733ac69a7b20d4d970b7d297a4899b97
Darek Margas <71180450+darek-margas@users.noreply.github.com>
2024-08-25 12:08:05 +1000

    Update __init__.py
    Actual fix for https://developers.home-assistant.io/docs/config_entries_index/

--- custom_components/flashforge_adventurer/__init__.py
+++ custom_components/flashforge_adventurer/__init__.py
@@ -15,12 +15,7 @@ async def async_setup_entry(
     hass.data[DOMAIN][entry.entry_id] = hass_data
 
     # Forward the setup to the sensor and camera platforms.
-    hass.async_create_task(
-        await hass.config_entries.async_forward_entry_setups(entry, 'sensor')
-    )
-    hass.async_create_task(
-        await hass.config_entries.async_forward_entry_setups(entry, 'camera')
-    )
+    await hass.config_entries.async_forward_entry_setups(entry, ["sensor", "camera"])
     return True
 
 
